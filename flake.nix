26ac114f8855b1206c9a9df4bf461a1a0b643ce5
Jeremy Wall <jeremy@marzhillstudios.com>
2022-03-03 17:45:22 -0500

    create nix package wrapper using flake-compat

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
     inputs = {
         flake-utils.url = "github:numtide/flake-utils";
         naersk.url = "github:nix-community/naersk";
+        flake-compat = { url = github:edolstra/flake-compat; flake = false; };
     };
 
-    outputs = {self, flake-utils, naersk}:
+    outputs = {self, flake-utils, naersk, flake-compat}:
         flake-utils.lib.eachDefaultSystem (system:
             let
                 naersk-lib = naersk.lib."${system}";
